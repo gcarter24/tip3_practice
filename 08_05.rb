c8859e6e545648d072ab64457cfefc76ed3a34bd
George <gchriscarter@gmail.com>
2021-08-05 22:04:30 -0400

    add 08_05 with practice solutions

---
+++ 08_05.rb
@@ -0,0 +1,24 @@
+# input -> [1,2,3,4]
+# output -> 24
+
+# 1x2x3x4
+# multiply all of the numbers in a given array
+# that will produce the product
+
+# input -> [2,2,2]
+# output -> 8
+
+# while loop
+#   increase index = i += 1
+
+# def multiply(array)
+#   i = 0
+#   product = 1
+#   while i < array.length
+#     product *= array[i]
+#     i += 1
+#   end
+#   return product
+# end
+
+# p multiply([1, 2, 3, 4])
